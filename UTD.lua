4fd8ad286cd88f21114dab0390bf59982a613af5
DanyGamerzz0 <jifijifiliski@gmail.com>
2026-01-20 20:11:01 +0100

    Update UTD.lua

--- UTD.lua
+++ UTD.lua
@@ -10,7 +10,7 @@ end
 
 local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/DanyGamerzz0/Rayfield-Custom/refs/heads/main/source.lua'))()
 
-local script_version = "V0.44"
+local script_version = "V0.45"
 
 local Window = Rayfield:CreateWindow({
    Name = "LixHub - Universal Tower Defense",
